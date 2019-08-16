65a6d2f752233e141a9bdeba0aa12d59f67faa42
capertuz <42881566+capertuz@users.noreply.github.com>
2019-08-16 13:07:33 -0400

    Update dog_years2.cpp

--- 2-variables/dog-years/dog_years2.cpp
+++ 2-variables/dog-years/dog_years2.cpp
@@ -1,29 +1,13 @@
-// Learn C++
-// Dog Years
-
 #include <iostream>
 
 int main() {
-    
-  std::string dog_name;
-  int dog_age;
-
-  int early_years, later_years, human_years;
-
-  std::cout << "Enter your dog's name: ";
-  std::cin >> dog_name;
-    
-  std::cout << "Enter your dog's age: ";
-  std::cin >> dog_age;
-    
-  early_years = 21;
   
-  later_years = (dog_age - 2) * 4;
-    
-  human_years = early_years + later_years;
-
-  std::cout << "My name is " << dog_name << "! Ruff Ruff, I am " << human_years << " years old in human years.\n";
-    
-  return 0;
-    
+  int dog_age = 3;//This is my dog age
+  int human_years= 0;
+  int early_years= 21;//The first two years of a dog’s life count as 21 human years
+  int later_years= ((dog_age-2) * 4);//Each following year counts as 4 human years
+  //std::cout<<"Enter the number of dog years:\n";
+  //std::cin>>dogYears;
+  human_years = later_years + early_years;
+  std::cout<<"My name is pluto! Ruff ruff, I am "<<human_years<<" years old in human years.\n";
 }
