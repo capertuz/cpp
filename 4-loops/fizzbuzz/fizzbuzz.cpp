cb0d6b2840a3675ddea643e1355075d46a7b5b00
capertuz <42881566+capertuz@users.noreply.github.com>
2019-08-16 13:25:13 -0400

    Update fizzbuzz.cpp

--- 4-loops/fizzbuzz/fizzbuzz.cpp
+++ 4-loops/fizzbuzz/fizzbuzz.cpp
@@ -2,28 +2,15 @@
 
 int main() {
 
-  for (int i = 1; i <= 100; i++) {
-
-    if (i % 15 == 0) {
-
-      std::cout << "FizzBuzz\n";
-
-    } else if (i % 5 == 0) {
-
-      std::cout << "Buzz\n";
-
-    } else if (i % 3 == 0) {
-
-      std::cout << "Fizz\n";
-
-    } else {
-
-      std::cout << i << "\n";
-
-    }
-
+  bool printed;
+  // Brain explodes here:
+  for(int i = 1; i<=100;i++)
+  {
+    printed = false;
+    if(i%3==0)  {std::cout << "Fizz";printed = true;}
+    if(i%5==0)  {std::cout << "Buzz";printed = true;}
+    if(!printed) std::cout << i;
+    std::cout << "\n";
   }
-
-  return 0;
 
 }
