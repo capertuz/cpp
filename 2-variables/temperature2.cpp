01eb9edd10d12ed3b7e8417b992c4b1b0b30f3c2
capertuz <42881566+capertuz@users.noreply.github.com>
2019-08-16 13:03:59 -0400

    Update temperature2.cpp

--- 2-variables/temperature2.cpp
+++ 2-variables/temperature2.cpp
@@ -6,14 +6,12 @@ int main() {
   double tempc;
   
   // Ask the user
-  std::cout << "Enter the temperature in Fahrenheit: ";
+  std::cout << "Enter the temperature in Fahrenheit:\n";
+  std::cin >> tempf; 
   
-  std::cin >> tempf;
   
   tempc = (tempf - 32) / 1.8;
   
   std::cout << "The temp is " << tempc << " degrees Celsius.\n";
-  
-  return 0;
   
 }
