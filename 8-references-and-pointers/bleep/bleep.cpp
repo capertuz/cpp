03b8135e4277a380831c0e26f04f81c51e124c33
capertuz <42881566+capertuz@users.noreply.github.com>
2019-08-16 14:49:13 -0400

    Update bleep.cpp

--- 8-references-and-pointers/bleep/bleep.cpp
+++ 8-references-and-pointers/bleep/bleep.cpp
@@ -1,22 +1,17 @@
-#include <iostream>
-#include <string>
-
+#include <iostream> 
+#include <string> 
 #include "functions.hpp"
 
-int main() {
-  
+
+int main()
+{
   std::string word = "broccoli";
+	std::string text = "I sometimes eat broccoli. The interesting thing about broccoli is that there are four interesting things about broccoli. Number One. Nobody knows how to spell it. Number Two. No matter how long you boil it, it's always cold by the time it reaches your plate. Number Three. It's green. #broccoli";
   
-  std::string sentence = "I sometimes eat broccoli. The interesting thing about broccoli is that there are four interesting things about broccoli. Number One. Nobody knows how to spell it. Number Two. No matter how long you boil it, it's always cold by the time it reaches your plate. Number Three. It's green. #broccoli";
+  bleep(word, text);
+
   
-  bleep(word, sentence);
+  std::cout<<text<<"\n";
   
-  for (int i = 0; i < sentence.size(); i++) {
-    
-    std::cout << sentence[i];
-    
-  }
-  
-  std::cout << "\n";
   
 }
