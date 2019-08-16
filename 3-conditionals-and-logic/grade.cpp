f29be4d27e117f9cb42b042ec4c998bf9fd02f08
capertuz <42881566+capertuz@users.noreply.github.com>
2019-08-16 13:14:42 -0400

    Update grade.cpp

--- 3-conditionals-and-logic/grade.cpp
+++ 3-conditionals-and-logic/grade.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 
 int main() {
-  
+
   int grade = 59;
-  
-  if (grade >= 60) {
-    
+
+  if (grade > 60) {
+
     std::cout << "Pass\n";
-    
-  } else {
-    
-    std::cout << "Fail\n";
-    
+
   }
   
-  return 0;
+  else {
 
+  std::cout << "Fail\n";
+
+}
+  
+  
+  
+  
 }
