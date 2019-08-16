22537daba335bd39307b8bd93178d1dfa8b93d3b
capertuz <42881566+capertuz@users.noreply.github.com>
2019-08-16 13:14:22 -0400

    Update pH.cpp

--- 3-conditionals-and-logic/pH.cpp
+++ 3-conditionals-and-logic/pH.cpp
@@ -4,20 +4,32 @@ int main() {
   
   double ph = 4.6;
   
-  if (ph > 7) {
-    
-    std::cout << "Basic\n";
-    
-  } else if (ph < 7) {
-    
-    std::cout << "Acidic\n";
-    
-  } else {
-    
-    std::cout << "Neutral\n";
-    
-  } 
+  // Write the if, else if, else here:
   
-  return 0;
+  if (ph > 7) {
+
+  std::cout << "Basic\n";
+
+} 
+else if (ph < 7) {
+
+  std::cout << "Acidic\n";
+
+}
+
+else {
+
+  std::cout << "Neutral\n";
+
+}
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
 }
