0d194fe45434e417a89b9b638b03bee4d40304ad
capertuz <42881566+capertuz@users.noreply.github.com>
2019-08-16 13:23:24 -0400

    Update guess.cpp

--- 4-loops/guess.cpp
+++ 4-loops/guess.cpp
@@ -2,25 +2,31 @@
 
 int main() {
 
-  int answer = 8;
-  
   int guess;
+  
+  int tries = 0;
 
   std::cout << "I have a number 1-10.\n";
   std::cout << "Please guess it: ";
   std::cin >> guess;
  
   // Write a while loop here:
-
-  while (guess != 8) {
-
-    std::cout << "Wrong guess, try again: ";
-    std::cin >> guess;
-
-  }
   
-  std::cout << "You got it!\n";
+  while (guess != 8 && tries < 50) {
 
-  return 0;
+  std::cout << "Wrong guess, try again: ";
+  std::cin >> guess;
+
+  tries++;
+
+}
+  
+  
+  
+  if (guess == 8) {
+    
+    std::cout << "You got it!\n";
+  
+  }  
   
 }
