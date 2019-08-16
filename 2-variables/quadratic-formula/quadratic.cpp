14c95c58798f6d1683ccd4088a417c20ccc2db3d
capertuz <42881566+capertuz@users.noreply.github.com>
2019-08-16 13:09:21 -0400

    Update quadratic.cpp

--- 2-variables/quadratic-formula/quadratic.cpp
+++ 2-variables/quadratic-formula/quadratic.cpp
@@ -2,25 +2,22 @@
 #include <cmath>
 
 int main() {
-
-  double a, b, c;
-  double root1, root2;
-
-  std::cout << "Enter a: ";
-  std::cin >> a;
-
-  std::cout << "Enter b: ";
-  std::cin >> b;
-
-  std::cout << "Enter c: ";
-  std::cin >> c;
-
-  root1 = (-b + std::sqrt(b*b - 4*a*c)) / (2*a);
-  root2 = (-b - std::sqrt(b*b - 4*a*c)) / (2*a);
-
-  std::cout << "Root 1 is " << root1 << "\n";
-  std::cout << "Root 2 is " << root2 << "\n";
-
-  return 0;
+  float a;
+  float b;
+  float c, root1, root2;
   
+  
+  std::cout<<"Enter a:\n";
+  std::cin >> a;
+  std::cout<<"Enter b:\n";
+  std::cin >> b;
+  std::cout<<"Enter c:\n";
+  std::cin >> c;
+  
+  root1 = (-b + std::sqrt(b*b - (4*a*c)))/(2*a);
+  root2 = (-b - std::sqrt(b*b - (4*a*c)))/(2*a);         
+  
+           std::cout<<"Root 1 is "<<root1<<"\n";
+           std::cout<<"Root 2 is "<<root2<<"\n";
+  return 0;
 }
