afdf8cade379aa301e4dc0378d9aa41efde9fd49
capertuz <42881566+capertuz@users.noreply.github.com>
2019-08-16 13:15:53 -0400

    Update coinflip.cpp

--- 3-conditionals-and-logic/coinflip.cpp
+++ 3-conditionals-and-logic/coinflip.cpp
@@ -4,19 +4,23 @@
 
 int main() {
   
+  // Create a number that's 0 or 1
+  
   srand (time(NULL));
   int	coin = rand() % 2;
   
-  if (coin == 0) {
-    
-    std::cout << "Heads\n";
-    
-  } else {
-	  
-    std::cout << "Tails\n";
-    
-  }
+  // If number is 0: Heads
+  // If it is not 0: Tails
   
-  return 0;
+  if (coin == 0) {
+  
+    std::cout << "Heads\n";
+  
+  }
+	else {
+	
+    std::cout << "Tails\n";
+  
+  }
   
 }
